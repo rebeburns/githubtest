63924bb3026d22442b5447c588020db6c4fcf523
Rebecca <Tarde@Alumno12>
2013-01-30 21:45:54 +0100

    Se ha enriquecido la salida del programa

---
+++ GitTest/src/GitClassTest.java
@@ -0,0 +1,12 @@
+
+public class GitClassTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("git test");
+		System.out.println("git test 2");
+	}
+
+}
